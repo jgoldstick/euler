13be895686755e91289e6e527b65319ea69c7aff
Joel Goldstick <joel.goldstick@gmail.com>
2016-03-24 12:28:11 -0400

    solved p8

--- p8.py
+++ p8.py
@@ -6,16 +6,23 @@ Find the greatest product of five consecutive digits in the 1000-digit number.
 candidates = []
 f = open("p8.txt")
 in_data = f.read(1500)
-in_data = map(int, list("".join(in_data.split())))
-print in_data
+data_1 = map(int, list("".join(in_data.split())))
+data_2 = map(int, list("".join(in_data.split())))
 
-max_candidate = 0
-while 1:
-    candidate = in_data[:5]
-    product = 1
-    for d in candidate:
-        product *= d
+
+def largest_product(n, data):
+    max_candidate = 0
+    while len(data) > n:
+        candidate = data[:n]
+        product = 1
+        for d in candidate:
+            product *= d
+
         if product > max_candidate:
             max_candidate = product
             print candidate, max_candidate, product
-    in_data.pop(0)
+        data.pop(0)
+    return max_candidate
+
+print largest_product(4, data_1)
+print largest_product(13, data_2)
